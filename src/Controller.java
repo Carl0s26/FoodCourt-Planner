e1bd38632140b07803ecd43c01fa7e7ad80de19b
Carl0s26 <Carloskepp26@gmail.com>
2024-03-01 09:07:19 -0400

    vaya
    bcuebcweiubcwe :(

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,77 @@
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+
 public class Controller {
-    
+
+    ArrayList<Object> listaDeMesas = new ArrayList<>();
+    int id =0;
+    int tableId;
+    int tableXcords;
+    int tableYcords;
+    boolean tableStatus;
+    public void Table(int tableYcords, int tableXcords, int tableId, boolean tableStatus) {
+        this.tableId = tableId;
+        this.tableXcords = tableXcords;
+        this.tableYcords = tableYcords;
+        this.tableStatus = tableStatus;
+    }
+
+    public int getTableId() {
+        return tableId;
+    }
+    public int getTableXcords() {
+        return tableXcords;
+    }
+    public int getTableYcords() {
+        return tableYcords;
+    }
+    public boolean getTableStatus() {
+        return tableStatus;
+    }
+    public void setTableId(int tableId) {
+        this.tableId = tableId;
+    }
+    public void setTableXcords(int tableXcords) {
+        this.tableXcords = tableXcords;
+    }
+    public void setTableYcords(int tableYcords) {
+        this.tableYcords = tableYcords;
+    }
+    public void setTableStatus(boolean tableStatus) {
+        this.tableStatus = tableStatus;
+    }
+
+
+    @FXML
+    private Label MenuLabel1;
+
+    @FXML
+    private Label MenuLabel2;
+
+    @FXML
+    private Label MenuLabel3;
+
+    @FXML
+    private Label MenuLabel4;
+
+    @FXML
+    private Label MenuLabel5;
+
+    @FXML
+    private Label MenuLabel6;
+
+    @FXML
+    private Label MenuLabel7;
+
+    @FXML
+    void EditButtonClicked(ActionEvent event) {
+        
+        
+    }
+
 }
