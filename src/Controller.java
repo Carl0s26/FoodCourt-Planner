964ceea1fb39f3e1b489b64ded01981aaf5dbc25
LordX1233 <sebastian@varillas.net>
2024-03-06 14:27:56 -0400

    A lot of shit

--- src/Controller.java
+++ src/Controller.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
@@ -95,21 +97,77 @@ public class Controller {
 
     @FXML
     void EditButtonClicked(ActionEvent event) {
-        Table(tableYcords, tableXcords+(id*2), id, tableStatus);
-        id +=1;
+        if (free){
+            editmode = true;
+            si.setImage(new Image("table3.png"));
+            free = false;
+            moves = true;
+        }else{
+            editmode = false;
+            si.setImage(new Image("table.png"));
+            free = true;
+            moves = false;
+        }
+    }
+
+    @FXML
+    void add_table(ActionEvent event) {
+        Table(tableYcords, tableXcords + 50, id, tableStatus);
+        id += 1;
         System.out.println(id);
     }
-    Boolean eggs = true;
+
+    Boolean free = true;
+    Boolean moves = false;
+    Boolean editmode = false;
+    
     @FXML
     void egg(MouseEvent event){
-        // if (eggs){
-        //     si.setImage(new Image("table1.png"));
-        //     eggs = false;
-        // }else{
-        //     si.setImage(new Image("table2.png"));
-        //     eggs = true;
-        // }
+        if (!editmode){
+            if (event.getButton() == MouseButton.SECONDARY){
+                if (free){
+                    si.setImage(new Image("table1.png"));
+                    free = false;
+                }else{
+                    si.setImage(new Image("table2.png"));
+                    free = true;
+                }
+            }
+        }
         
+    }
+    @FXML
+    void drag(MouseEvent event) {
+        if(moves){
+
+            if (event.getSceneX() < 10.00){
+                si.setLayoutX(10.0);
+            }else if (event.getSceneX() > 769.00){
+                si.setLayoutX(769.0);
+
+
+
+            
+            }else{    
+                si.setLayoutX(event.getSceneX());
+            }
+
+
+
+            if (event.getSceneY() > 515.00){
+                si.setLayoutY(515.0);
+            }else if (event.getSceneY() < 8.00){
+                si.setLayoutY(8.0);
+            }else if (event.getSceneY() < 41.00){
+                si.setLayoutY(41.0);
+
+            }else{    
+                si.setLayoutY(event.getSceneY());
+            }
+
+            System.out.println(event.getSceneY());
+        }
+         
     }
 
 }
