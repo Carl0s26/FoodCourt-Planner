9216433822e485160fb5138ee90af4c6da28b7aa
LordX1233 <sebastian@varillas.net>
2024-03-09 13:36:04 -0400

    wow

--- src/Login_controller.java
+++ src/Login_controller.java
@@ -1,10 +1,17 @@
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 // import java.io.File;
+
+
 public class Login_controller {
+
+    public static Boolean off = false;
 
     @FXML
     private Button Login_button;
@@ -16,8 +23,15 @@ public class Login_controller {
     private PasswordField User_name_login;
 
     @FXML
-    void Onbuttonclick(ActionEvent event) {
-
+    void login(ActionEvent event) {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();  
+            off = true;
+        } catch (Exception e) {}
     }
-
 }
