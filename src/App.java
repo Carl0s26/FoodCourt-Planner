126c3fdda0033a4f6864425b54f144be258cd9a6
Carl0s26 <Carloskepp26@gmail.com>
2024-03-11 13:41:12 -0400

    cositas

--- src/App.java
+++ src/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login_window.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Sign_up_window.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- src/Sign_up_controller.java
+++ src/Sign_up_controller.java
@@ -23,19 +23,35 @@ public class Sign_up_controller {
     private PasswordField Sign_up_confirm_password;
     
     @FXML
-    void Onbuttonclick(ActionEvent event) {
+    void OnSignUpbuttonclick(ActionEvent event) {
         //5accounts(str_Username, str_Password);
         String str_Gmail = Sign_up_Gmail.getText();
         String str_Password = Sign_up_Password.getText();
+        String str_ConfirmPassword = Sign_up_confirm_password.getText();
         String str_Username = Sign_up_Username.getText();
-
+        if (!str_Password.equals(str_ConfirmPassword)) {
+            // gives a error Passwords don't match
+            System.out.println("The pasword and the pasword confirmation must match");
+        }
+        else if (str_Username == null || str_Username == "" || str_Password == null || str_Password == "" || str_Username == " " || str_Password == " "){
+            // gives you an error passwords/usernames are empty.
+            System.out.println("The pasword or name can't be blanck");
+        }
+        else{
+            accounts(str_Username, str_Password);
+        }
+        //accounts(str_Username, str_Password);
     }
 
     public void accounts(String str_username, String str_Password){
         try {
-            FileWriter myObj = new FileWriter("accounts.txt");
+            FileWriter myObj = new FileWriter("accounts.txt", true);
             myObj.write(str_username);
-
+            myObj.write(" ");
+            myObj.write(str_Password);
+            myObj.write(" ");
+            myObj.write("client");
+            myObj.write(System.lineSeparator());
             myObj.close();
         } catch (Exception e) {
             System.out.println("Something happened " + e);
